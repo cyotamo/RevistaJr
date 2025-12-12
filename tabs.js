8efe7b0f62c316e495a739a1936b849782bb393c
cyotamo <cyotamo@yahoo.com.br>
2025-12-12 14:49:39 +0200

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+ document.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll("nav a");
   const sections = document.querySelectorAll(".tab");
 
@@ -20,24 +20,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // deixar INÍCIO activo no carregamento
   document.querySelector('a[data-tab="inicio"]').classList.add("active");
-});
 
-// ===== LÓGICA DO MODAL LOGIN =====
-const btnLogin = document.querySelector(".login-btn");
-const modal = document.getElementById("modalLogin");
-const btnFechar = document.getElementById("btnFecharModal");
+  // ===== LÓGICA DO MODAL LOGIN =====
+  const btnLogin = document.querySelector(".login-btn");
+  const modal = document.getElementById("modalLogin");
+  const btnFechar = document.getElementById("btnFecharModal");
 
-btnLogin.addEventListener("click", () => {
-  modal.style.display = "flex";  // mostrar modal
-});
+  btnLogin.addEventListener("click", () => {
+    modal.style.display = "flex";  // mostrar modal
+  });
 
-btnFechar.addEventListener("click", () => {
-  modal.style.display = "none";  // fechar modal
-});
+  btnFechar.addEventListener("click", () => {
+    modal.style.display = "none";  // fechar modal
+  });
 
-// Fechar ao clicar fora da caixa
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-  }
+  // Fechar ao clicar fora da caixa
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
 });
