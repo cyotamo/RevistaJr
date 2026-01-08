bd84096512f9d861f72d2bb2535ba9c7b8229528
cyotamo <cyotamo@yahoo.com.br>
2026-01-08 16:48:54 +0200

    Revert "Update sharing metadata and PWA assets"
    This reverts commit fd3d273b1565732035df75fc48653fbfabe5c180.

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,7 @@
-const CACHE_NAME = "econogest-cache-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/tabs.js",
-  "/submissao.js",
-  "/firebase-config.js",
-  "/favicon.png"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
-  );
+  event.respondWith(fetch(event.request));
 });
